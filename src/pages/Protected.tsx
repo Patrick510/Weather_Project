e25ce9c1b8614a79f50281b001e63ae191363526
Patrick510 <patrickdedinho321xz@gmail.com>
2025-02-05 00:01:53 -0400

    fix: redirect to home instead of login for unauthorized access in ProtectedRoute

--- src/pages/Protected.tsx
+++ src/pages/Protected.tsx
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export function ProtectedRoute() {
   const user = JSON.parse(localStorage.getItem("loggedUser") || "null");
 
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  return user ? <Outlet /> : <Navigate to="/" />;
 }
